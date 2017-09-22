a459ccb408436200191a351be7964989c05b379c
Olaolu Olawuyi <whiskydee@gmail.com>
2017-09-22 12:25:51 +0100

    Remove unwanted files

--- gulpfile.babel.js
+++
@@ -1,9 +0,0 @@
-import gulp from 'gulp';
-import gulpStats from 'gulp-stats';
-import tasks from './.gulp';
-
-// trigger gulp-stats
-gulpStats(gulp);
-
-// initialize default task
-gulp.task('default', tasks);
